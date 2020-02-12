9123b7f7b07de6d6b22bed72d857e0d65643d934
88-Rising <348514635@qq.com>
2020-02-12 12:47:56 +0800

    使用测试例测试输入一个文件名获取全拼和拼音首字母增加拼音去重和拼音字符串合并功能

--- src/main/java/util/PinyinUtil.java
+++ src/main/java/util/PinyinUtil.java
@@ -146,5 +146,7 @@ public class PinyinUtil {
         System.out.println(Arrays.toString(get("中华人民共和国")));
         System.out.println(Arrays.toString(get("中华1人民b共和A国")));
 
+        System.out.println(Arrays.toString(compose(get("中华人民共和国",true))));
+
     }
 }
