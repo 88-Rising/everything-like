2f61f0eca7f89a34f9b889e9f578138c4806b5bc
88-Rising <348514635@qq.com>
2020-02-20 10:49:39 +0800

    完善文件属性类

--- src/main/java/app/FileMeta.java
+++ src/main/java/app/FileMeta.java
@@ -1,4 +1,111 @@
 package app;
 
+import util.DBUtil;
+import util.PinyinUtil;
+
+import java.io.File;
+import java.util.Date;
+
 public class FileMeta {
+
+    private String name;//文件名称
+    private String path;//文件所在父目录的路径
+    private Long size;//文件大小
+    private Date lastModified;//文件上次修改时间
+
+
+    //给客户端控件使用，和app.fxml中定义的名称一致
+    private String sizeText;
+    //和app.fxml中的定义一样
+    private String lastModifiedText;
+    //文件名拼音
+    private String pinyin;
+    //文件名拼音首字母
+    private String pinyinFirst;
+
+    //通过文件设置属性
+    public FileMeta(File file){
+         this(file.getName(), file.getParent(), file.length(), new Date(file.lastModified()));
+
+    }
+
+    //通过数据库获取的数据设置FileMeta
+    public FileMeta(String name, String path, Long size, Date lastModified){
+        this.name=name;
+        this.path=path;
+        this.size=size;
+        this.lastModified=lastModified;
+        if(PinyinUtil.containsChinese(name)){
+            String[] pinyins=PinyinUtil.get(name);
+            pinyin=pinyins[0];
+            pinyinFirst=pinyins[1];
+        }
+        //客户端表格空间文件大小，文件上次修改时间设置
+//        sizeText=?
+//          lastModified=?
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public Date getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Date lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public String getSizeText() {
+        return sizeText;
+    }
+
+    public void setSizeText(String sizeText) {
+        this.sizeText = sizeText;
+    }
+
+    public String getLastModifiedText() {
+        return lastModifiedText;
+    }
+
+    public void setLastModifiedText(String lastModifiedText) {
+        this.lastModifiedText = lastModifiedText;
+    }
+
+    public String getPinyin() {
+        return pinyin;
+    }
+
+    public void setPinyin(String pinyin) {
+        this.pinyin = pinyin;
+    }
+
+    public String getPinyinFirst() {
+        return pinyinFirst;
+    }
+
+    public void setPinyinFirst(String pinyinFirst) {
+        this.pinyinFirst = pinyinFirst;
+    }
 }
